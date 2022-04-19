474352f71b8c1b22f6da1db5e6facfe75d0ce8f4
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-04-19 22:05:05 +0900

    Update MultiHeadAttention.py

--- model/MultiHeadAttention.py
+++ model/MultiHeadAttention.py
@@ -156,35 +156,23 @@ class MultiHeadAttention_CF(nn.Module):
 
     def forward(self, x): #def forward(self, x, mask=None):
         batch_size = x.shape[0]
-        
-        # query, key, value -> Q, K, V  
-        Q, K, V = self.weight_q(x), self.weight_k(x), self.weight_v(x)
-        
-        ## multi-head attention (d_model -> n_head*head_dim 형태로 변환) / (n_head개 마다 각각 head_dim을 갖도록      ##[batch, length, n_head, head_dim] -> [batch, n_head, length, head_dim]
+         
+        Q, K, V = self.weight_q(x), self.weight_k(x), self.weight_v(x)   
         Q = Q.view(batch_size, -1, self.n_head, self.head_dim).permute(0, 2, 1, 3)
         K = K.view(batch_size, -1, self.n_head, self.head_dim).permute(0, 2, 1, 3)
         V = V.view(batch_size, -1, self.n_head, self.head_dim).permute(0, 2, 1, 3)
-        # -1 => 나머지 값들에 의해 결정됨
         
         #=================================================================================================
-        ## compute similarity(dot-product)      ##[batch, n_head, query_len, key_len]
         attn_score = torch.matmul(Q, K.permute(0,1,3,2)) / self.scale
-        
         ## option_masking 
         #if mask is not None:
         #    mask = mask.unsqueeze(1)
         #    attn_score = attn_score.masked_fill(mask == 0, -1e10)
-            
         attn_dstn = torch.softmax(attn_score, dim=-1)
-        # dim=-1 => 현재 input이 tensor(vector였으면 dim 지정 필요 x) last dimension에 대하여 softmax 적용
-        # ex. nn.Softmax(dim=-1) torch.randn(2,3) -> 3     
-                 
-        ## scaled dot-product attention
         out = torch.matmul(self.dropout(attn_dstn), V)
         #=================================================================================================
         out = out.permute(0,2,1,3).contiguous()
         out = out.view(batch_size, -1, self.d_model)
         out = self.fc_concat(out)
-        #out = self.dropout(out)
         
         return out
