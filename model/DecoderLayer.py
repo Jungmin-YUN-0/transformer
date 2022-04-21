a78e7ae82c447b69df1c39c1c9e812877c35f810
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-04-21 15:08:27 +0900

    Update DecoderLayer.py

--- model/DecoderLayer.py
+++ model/DecoderLayer.py
@@ -4,7 +4,6 @@ from model.FeedForward import FeedForward
 
 # decoder layer
 
-##masked_, cross_, feedforward_
 class DecoderLayer(nn.Module):
     def __init__(self, d_model, n_head, ffn_hidden, drop_prob, device, attn_option, n_position):
         super().__init__()
@@ -28,22 +27,16 @@ class DecoderLayer(nn.Module):
 
         # 1-2# dropout, residual connection, layer normalization
         trg = trg + self.self_attn_layer_norm(self.dropout1(_trg))
-        #x = self.self_attn_layer_norm(trg + self.dropout1(_trg)) #!#
         
         # 2-1# cross multi-head attention (encoder-decoder attention)
         _trg, attention = self.enc_dec_attention(trg, enc_src, enc_src, src_mask)
-
         # 2-2# dropout, residual connection, layer normalization
         trg = trg + self.enc_dec_layer_norm(self.dropout2(_trg))
-        #x = self.enc_dec_layer_norm(x + self.dropout2(_trg))
         
         # 3-1# feed-forward neural network
         _trg = self.ffn(trg)
         # 3-2# dropout, residual connection, layer normalization
         trg = trg + self.ffn_layer_norm(self.dropout3(_trg))
-        #x = self.ffn_layer_norm(x + self.dropout3(_trg)) #!#
+
 
         return trg, attention
-
-
-        #src = src + self.attn_layer_norm(self.dropout1(_src)) #!#
