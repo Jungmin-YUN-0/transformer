69a15a4c361b061cc5b775852ce1dc5b3add185c
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-05-04 14:21:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from test import Test
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-gpu', default="3", help="# of gpu")
+parser.add_argument('-gpu', default="0", help="# of gpu")
 parser.add_argument('-option', default='CT', help="BASE(vanilla transformer) / LR(low-rank attention) / CT(core-token attention) ")
 parser.add_argument('-task',  default='TRAIN', help="TRAIN / TEST ")
-parser.add_argument('-data_task', default="CF", help="MT(machine translation) / CF(classification)" )
+parser.add_argument('-data_task', default="MT", help="MT(machine translation) / CF(classification)" )
 
-parser.add_argument('-data_pkl', default="data_imdb_256_CF_0419.pickle", help="file name of preprocessed data(pickle file)")
-parser.add_argument('-model_save', default="classification_CT.pt", help="name of saved(updated) model")
-parser.add_argument('-pred_save', default="test_save.txt", help="name of file containing prediction result")
+parser.add_argument('-data_pkl', default="machinetranslation_wmt16_512.pickle", help="file name of preprocessed data(pickle file)")
+parser.add_argument('-model_save', default="transformer_BASE.pt", help="name of saved(updated) model")
+parser.add_argument('-pred_save', default="transformer_BASE.txt", help="name of file containing prediction result")
 
 parser.add_argument('-batch_size', default=16, type=int, help="batch size")
 parser.add_argument('-n_epoch', default=30, type=int, help="# of epoch")
 parser.add_argument('-learning_rate', default=1e-4, type=float, help="learning rate") #1e-5 for test
 parser.add_argument('-num_warmup', default=4000, type=int, help="# of warmup (about learning rate)") # 2000으로 test (transformer_lr1e-3_64.pt)
 
-parser.add_argument('-hidden_dim', type=int, default= 512, help="hidden dimension(=d_model")
-parser.add_argument('-n_layer', type=int, default=  6, help="# of encoder&decoder layer")
-parser.add_argument('-n_head', type=int, default= 8, help="# of head(about multi-head attention)")
-parser.add_argument('-ff_dim', type=int, default= 2048, help="dimension of feed-forward neural network")
-parser.add_argument('-dropout', type=float,default= 0.1, help="ratio of dropout")
+parser.add_argument('-hidden_dim', type=int, default=512, help="hidden dimension(=d_model")
+parser.add_argument('-n_layer', type=int, default=6, help="# of encoder&decoder layer")
+parser.add_argument('-n_head', type=int, default=8, help="# of head(about multi-head attention)")
+parser.add_argument('-ff_dim', type=int, default=2048, help="dimension of feed-forward neural network")
+parser.add_argument('-dropout', type=float,default=0.1, help="ratio of dropout")
 
 opt = parser.parse_args()
 
